177d52c2367ea0c16f9cb7d23257bd21005ac6b4
Effi-S <effi.szt@gmail.com>
2022-01-11 12:20:03 +0200

    minor edit

--- calory-count/plotting.py
+++ calory-count/plotting.py
@@ -1,7 +1,6 @@
 """This module holds different plotting functions that are compatible kivy."""
 from __future__ import annotations
 
-from kivy.metrics import dp
 from kivymd_extensions.akivymd.uix.charts import AKPieChart
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
@@ -27,14 +26,10 @@ def plot_pie_chart(data: dict[str, float],
 def plot_graph(data: dict[str, float],
                x_label: str = None, y_label: str = 'Y',
                ) -> FigureCanvasKivyAgg:
-    data = {'2022-01-11': 100,
-            '2022-01-10': 100,
-            '2022-01-09': 300,
-            '2022-01-08': 650,
-            '2022-01-07': 500,
-            '2022-01-06': 600,
-            '2022-01-05': 780,
-            '2022-01-04': 800}
+    """Plot a graph of dates to values.
+    e.g. plot_graph(data = {'2022-01-11': 100,
+                            '2022-01-10': 100,
+                            '2022-01-09': 300}, y_label='Calories')"""
 
     plt.rcParams.update({
         "figure.facecolor": (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 30%
@@ -42,7 +37,7 @@ def plot_graph(data: dict[str, float],
         "savefig.facecolor": (0.0, 0.0, 1.0, 0.2),  # blue  with alpha = 20%
         "figure.autolayout": True
     })
-    data = {'-'.join(k.split('-')[1:]): v for k, v in data.items()} # removing year
+    data = {'-'.join(k.split('-')[1:]): v for k, v in data.items()}  # removing year
     plt.plot(*zip(*data.items()), label=y_label)
     plt.legend()
 
@@ -51,8 +46,6 @@ def plot_graph(data: dict[str, float],
 
     ax = plt.gca()
     ax.grid(True)
-    # for label in ax.get_xticklabels():
-    #     label.set_rotation(45)
     ax.yaxis.tick_right()
     ax.xaxis.tick_top()
     ax.tick_params(axis='x', colors='green')
