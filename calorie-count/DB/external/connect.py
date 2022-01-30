0a7b1b31e5a940cb551c8cf5ee8486bec3108242
Effi-S <effi.szt@gmail.com>
2022-01-30 23:13:41 +0200

    Added ExternalFoodsDB with custom search

---
+++ calorie-count/DB/external/connect.py
@@ -0,0 +1,86 @@
+from __future__ import annotations
+import atexit
+import sqlite3
+from dataclasses import dataclass, asdict, astuple
+from typing import Generator
+from difflib import SequenceMatcher
+
+
+def similarity(a: str, b: str) -> float:
+    """Get similarity between 2 strings based on diff-lib's SequenceMatcher ratio"""
+    return SequenceMatcher(None, str(a), str(b)).ratio()
+
+
+@dataclass
+class FoodData:
+    """This class represents a Searchable Food """
+    description: str
+    portions: str  # string representation of mapping portion to quantity(g)  e.x - 'cup:30,bowl:100'...
+    protein: float
+    fats: float
+    carbs: float
+    sodium: float
+    sugar: float
+    water: float
+    
+    def __post_init__(self):
+        self.description = self.description.replace('"', '')
+
+    def portions_dict(self) -> dict[str, float]:
+        return {a: b for x in ','.split(self.portions) for a, b in ':'.split(x)}
+
+
+class ExternalFoodsDB:
+    def __init__(self):
+        self.conn = sqlite3.connect('external_foods')
+        atexit.register(lambda: self.conn.close())  # In-case 'with' not used
+        self.cursor = self.conn.cursor()
+        self.cursor.execute('''CREATE TABLE if not exists foods(
+                                description text,
+                                portions text,
+                                protein real,
+                                fats real,
+                                carbs real,
+                                sodium real,
+                                sugar real,
+                                water real
+                            )''')
+        self.conn.commit()
+        self.conn.create_function('edit_dist', 2, similarity)
+        self.conn.commit()
+
+    def __enter__(self, *a, **k):
+        return self
+
+    def __exit__(self, *a, **k):
+        self.conn.close()
+
+    def add_food(self, food: FoodData):
+        """Here we add a Food, parsed from an external API/JSON into ExternalFoodsDB."""
+        cmd = f'INSERT INTO foods Values {astuple(food)}'
+        print(cmd, asdict(food))
+        self.cursor.execute(cmd, asdict(food))
+        self.conn.commit()
+
+    def get_similar_food_by_name(self, name: str, max_results: int = 5) -> Generator[FoodData]:
+        """Given a name of a food return the most similar food in the DB.
+        Ordered most similar to least similar.
+        By default maximum of 5 values in the list, override 'max_results' to change this.
+
+        Algorithm of similarity:
+            1. Get foods where the given name is contained in the description.
+            2. If none found in 1. -  iterate row-by-row running edit-distance on them
+            add those that are > 0.9 ration.
+            (Note: SQLite has 'editdist3' but I don't think it can work on android) """
+        self.cursor.execute(f"SELECT * FROM foods WHERE description LIKE '%{name}%'")
+        count = 0
+        for _, row in zip(range(max_results), self.cursor):
+            food = FoodData(*row)
+            yield food
+            count += 1
+
+        if count < max_results:
+            self.cursor.execute(f"SELECT * FROM foods WHERE edit_dist(`description`,  '{name}') >= 0.9")
+            for _, row in zip(range(max_results - count), self.cursor):
+                food = FoodData(*row)
+                yield food
---
+++ calorie-count/DB/external/parsing.py
@@ -0,0 +1,59 @@
+"""This module is used to parse external data and add them to the DB for the app."""
+from __future__ import annotations
+from __future__ import absolute_import
+import os
+import time
+from typing import Generator
+
+import ijson
+
+from connect import FoodData
+
+from connect import ExternalFoodsDB
+
+
+def parse_foods_foundation(filename: str, heading: str) -> Generator[FoodData]:
+    data = ijson.items(open(filename, errors='ignore'), f'{heading}.item')
+    for i, food in enumerate(data):
+        description = food['description']
+        if 'servingSize' in food:
+            unit = food['servingSizeUnit']
+            if unit in ('mg', 'ml'):
+                food['servingSize'] /= 1000
+            elif unit in ('g', 'l'):
+                pass
+            else:
+                assert False, f'Unknown serving' + unit
+            portions = {'Serving': food['servingSize']}
+        elif 'foodPortions' in food:
+            portions = food['foodPortions']
+            portions = {p['measureUnit']['name']: p['gramWeight'] for p in portions}
+        else:
+            continue
+        nut_dict = dict.fromkeys(('protein', 'fats', 'carbs', 'sodium', 'sugar', 'water'), .0)
+        for nut in food['foodNutrients']:
+            name = nut['nutrient']['name']
+            unit_name = nut['nutrient']['unitName']
+            for s in nut_dict:
+                if s.rstrip('s') in name.lower():
+                    nut_dict[s] = float(nut['amount'])
+                    if unit_name == 'mg':
+                        nut_dict[s] /= 1000
+
+        portions = ','.join(f'{k}:{v}' for k, v in portions.items())
+        yield FoodData(description=description, portions=portions, **nut_dict)
+
+
+if __name__ == '__main__':
+
+    db_dict = {'FoodData_Central_foundation_food_json_2021-10-28.json': 'FoundationFoods',
+               'FoodData_Central_sr_legacy_food_json_2021-10-28.json': 'SRLegacyFoods',
+               'FoodData_Central_branded_food_json_2021-10-28.json': 'BrandedFoods',
+               }
+    db_dict = {os.path.abspath(k): v for k, v in db_dict.items()}
+    with ExternalFoodsDB() as fdb:
+        start = time.perf_counter()
+        for path, header in db_dict.items():
+            for f in parse_foods_foundation(path, header):
+                fdb.add_food(f)
+        print(' ..Done.. Took', time.perf_counter() - start)
