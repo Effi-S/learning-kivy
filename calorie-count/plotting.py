149aa0557b23f0d65e18f4add2c479b19d465959
Effi-S <effi.szt@gmail.com>
2022-02-07 02:49:42 +0200

    AkivyMD swapped with matplotlib

--- calorie-count/plotting.py
+++ calorie-count/plotting.py
@@ -1,37 +1,43 @@
 """This module holds different plotting functions that are compatible kivy.
 
 Building Notes:
-    1) akivymd from:
-        - run: pip install kivymd-extensions.akivymd (checked on version 1.2.6)
-        - To buildozer.spec add: requirements = https://github.com/kivymd-extensions/akivymd/archive/main.zip
-    2) Add matplotlib to requirements in buildozer.spec
+    1) Add matplotlib to requirements in buildozer.spec
         - run pip install matplotlib (checked on version 3.2.2)
         - To buildozer.spec add: requirements = matplotlib==3.2.2
-    3) Get matplotlib backend package to kmplot folder
+    2) Get matplotlib backend package to kmplot folder
             - pip install kivy.garden / sudo apt-get install kivy_garden
             - garden install matplotlib
                 (any issues see:  https://www.youtube.com/watch?v=ak6HwZyj1lM&ab_channel=SkSahil)
 """
 
 from __future__ import annotations
-
-from kivymd_extensions.akivymd.uix.charts import AKPieChart
 from kmplot.backend_kivyagg import FigureCanvasKivyAgg
 import matplotlib.pyplot as plt
 
+_RC_PARAMS = {  # params for plotting
+    "figure.facecolor": (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 30%
+    "axes.facecolor": (0.0, 1.0, 0.0, 0.0),  # green with alpha = 0%
+    "savefig.facecolor": (0.0, 0.0, 1.0, 0.2),  # blue  with alpha = 20%
+    "figure.autolayout": True
+}
 
-def plot_pie_chart(data: dict[str, float]) -> AKPieChart:
+
+def plot_pie_chart(data: dict[str, float]) -> FigureCanvasKivyAgg:
     """This function takes in a data dict name->quantity and returns a AKPieChart"""
-    sum_values = sum(data.values()) or .0000001  # (delta to avoid zero devision)
-    data = {k: (v / sum_values) * 100 for k, v in data.items()}
-    sum_values = sum(data.values())
-    leftover = 100 - sum_values
-    data[list(data.keys())[-1]] += leftover
 
-    chart = AKPieChart(
-        items=[data],
-    )
-    return chart
+    plt.rcParams.update(_RC_PARAMS)
+    fig, ax = plt.subplots(figsize=(24, 12))
+
+    sum_ = sum(data.values())
+    data = {k: (v/sum_)*100 for k, v in data.items()}  # values to percents
+
+    _g = (f'{k}: {v: .1f}%' for k, v in data.items())
+    ax.pie(data.values(), autopct=lambda *a, **k: next(_g),
+           textprops={'color': 'w'})
+    ax.axis('normal')
+    canvas = FigureCanvasKivyAgg(fig)
+    plt.close()  # clearing memory of plt
+    return canvas
 
 
 def plot_graph(data: dict[str, float],
@@ -42,12 +48,7 @@ def plot_graph(data: dict[str, float],
                             '2022-01-10': 100,
                             '2022-01-09': 300}, y_label='Calories')"""
 
-    plt.rcParams.update({
-        "figure.facecolor": (1.0, 0.0, 0.0, 0.0),  # red   with alpha = 30%
-        "axes.facecolor": (0.0, 1.0, 0.0, 0.0),  # green with alpha = 0%
-        "savefig.facecolor": (0.0, 0.0, 1.0, 0.2),  # blue  with alpha = 20%
-        "figure.autolayout": True
-    })
+    plt.rcParams.update(_RC_PARAMS)
     data = {'-'.join(k.split('-')[1:]): v for k, v in data.items()}  # removing year
     if len(data) == 1:
         plt.bar(*zip(*data.items()), label=y_label)
@@ -69,4 +70,3 @@ def plot_graph(data: dict[str, float],
     canvas = FigureCanvasKivyAgg(plt.gcf())
     plt.close()  # clearing memory of plt
     return canvas
-
